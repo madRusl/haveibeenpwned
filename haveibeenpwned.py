835dbec391b44de884d5c4addd468ce7fcc49bf1
ruslan al-dzharakh <mod1000slint@gmail.com>
2020-01-13 00:21:47 +0300

    fixes

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -12,17 +12,19 @@ r = requests.get(f"https://api.pwnedpasswords.com/range/{password_hash_prefix}")
 s = r.text
 output = s.splitlines()
 
+
 def hash_check():
     for i in output:
-        if password_hash_root == i.split(':', 1)[0]:
-            print(f'Your password \'{password}\' has been leaked')
-            print('Hash digest matched', i.split(':', 1)[1], 'times')
+        if password_hash_root == i.split(":", 1)[0]:
+            print(f"Your password '{password}' has been leaked")
+            print("Hash digest matched", i.split(":", 1)[1], "times")
             return True
         else:
             pass
     return False
 
+
 if hash_check() is False:
-    print('Password hasn\'t been leaked')
+    print("Password hasn't been leaked")
 
 password = None
