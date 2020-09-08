535f53b8ec0e2750d8868af077abb864e68a4af8
ruslan al-dzharakh <ruslanal-dzharakh@coherentsolutions.com>
2020-09-08 16:27:07 +0300

    upd

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -8,6 +8,12 @@ password_hash = hashlib.sha1(password.encode()).hexdigest().upper()
 password_hash_prefix = password_hash[:5]
 password_hash_root = password_hash[5:]
 
+
+r = requests.get(f"https://api.pwnedpasswords.com/range/{password_hash_prefix}")
+s = r.text
+output = s.splitlines()
+
+
 def hash_check():
     for i in output:
         if password_hash_root == i.split(":", 1)[0]:
